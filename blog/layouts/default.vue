aed27fc3fa44f7c39d6fa120c6b9ba43046ce951
yang.yang <yang.yang@karakal.com.cn>
2019-05-16 17:47:03 +0800

    111

--- blog/layouts/default.vue
+++ blog/layouts/default.vue
@@ -18,7 +18,7 @@
       return {
         title: 'mocc的博客-个人的心得与分享',
         meta: [
-          {hid: 'description', name: 'description', content: 'mocc的个人博客，是一个记录博主学习和成长的自媒体博客。关注于java全栈技术的学习。'},
+          {hid: 'description', name: 'description', content: '杨洋（mocc）的个人博客，是一个记录博主学习和成长的自媒体博客。关注于java全栈技术的学习。'},
           {hid: 'keywords', name: 'keywords', content: 'mocc,杨洋,互联网,自媒体,杨洋博客,科技博客,独立博客,个人博客,原创博客,java,spring,springBoot'},
           {hid: 'author', content: 'mocc'}
         ]
